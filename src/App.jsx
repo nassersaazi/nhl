8fe10711c208b3059674ba74e1b2af50fb73eb4f
nassersaazi <nassersnoop@gmail.com>
2023-11-19 00:50:56 +0300

    Restructure project - Navbar v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import { Nav } from "./components";
-import {
-  CustomerReviews,
-  Footer,
-  Hero,
-  Products,
-  About,
-  RequestQuote
-} from "./sections";
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <main className='relative'>
-      <Nav />
-      <section className='xl:padding-l wide:padding-r padding-b'>
-        <Hero />
-      </section>
-      <section className='padding'>
-        <About />
-      </section>
-      <section className='padding-x py-10'>
-        <Products />
-      </section>
-      <section className='padding'>
-        <RequestQuote />
-      </section>
-      <section className='bg-pale-blue padding'>
-        <CustomerReviews />
-      </section>
-      <section className=' bg-black padding-x padding-t pb-8'>
-        <Footer />
-      </section>
-    </main>
+     <div>
+      <Navbar />
+    </div>
   );
 };
 
